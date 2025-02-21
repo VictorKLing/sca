2fd3db51a71aca0a3ae00201295827eaad6111ea
Victor Kling <victorkling.p@gmail.com>
2025-02-21 16:29:34 -0300

    coomit 5

---
+++ src/assets/js/script.js
@@ -0,0 +1,44 @@
+document.addEventListener("DOMContentLoaded", function() {
+  document.querySelectorAll(".faq-button").forEach(button => {
+      button.addEventListener("click", function() {
+          const content = this.nextElementSibling;
+          const icon = this.querySelector("svg");
+
+          if (content.style.maxHeight && content.style.maxHeight !== "0px") {
+              content.style.maxHeight = "0px";
+          } else {
+              content.style.maxHeight = content.scrollHeight + "px";
+          }
+          icon.classList.toggle("rotate-90");
+      });
+  });
+
+  const menu = document.getElementById('side-menu');
+  const overlay = document.getElementById('menu-overlay');
+  const body = document.body;
+
+  document.getElementById('menu-toggle').addEventListener('click', function () {
+      menu.classList.remove('translate-x-full');
+      menu.classList.add('translate-x-0');
+      
+      overlay.classList.remove('translate-x-full');
+      overlay.classList.add('translate-x-0');
+
+      body.classList.add('overflow-hidden');
+  });
+
+  document.getElementById('close-menu').addEventListener('click', function () {
+      menu.classList.remove('translate-x-0');
+      menu.classList.add('translate-x-full');
+      
+      overlay.classList.remove('translate-x-0');
+      overlay.classList.add('translate-x-full');
+
+      body.classList.remove('overflow-hidden');
+  });
+
+  // fecha o menu ao clicar no overlay
+  overlay.addEventListener('click', function () {
+      document.getElementById('close-menu').click();
+  });
+});
--- src/assets/js/swiper.js
+++ src/assets/js/swiper.js
@@ -7,3 +7,18 @@ var swiper = new Swiper(".mySwiper", {
     clickable: true,
   },
 });
+
+var swiper = new Swiper(".mySwiper-clientes", {
+  slidesPerView: 3,
+  loop: true,
+  spaceBetween: 30,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  breakpoints: {
+    1024: {
+      slidesPerView: 6,
+    },
+  },
+});
