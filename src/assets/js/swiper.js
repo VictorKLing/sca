08bf7e7ce64baf4fd07288d8cbcadca2ab8f0976
Victor Kling <victorkling.p@gmail.com>
2025-02-28 01:04:00 -0300

    ajustes finais

--- src/assets/js/swiper.js
+++ src/assets/js/swiper.js
@@ -1,15 +1,5 @@
 var swiper = new Swiper(".mySwiper", {
-  slidesPerView: 4,
-  loop: true,
-  spaceBetween: 30,
-  pagination: {
-    el: ".swiper-pagination",
-    clickable: true,
-  },
-});
-
-var swiper = new Swiper(".mySwiper-clientes", {
-  slidesPerView: 3,
+  slidesPerView: 1, // Valor padrão para telas menores
   loop: true,
   spaceBetween: 30,
   pagination: {
@@ -17,10 +7,33 @@ var swiper = new Swiper(".mySwiper-clientes", {
     clickable: true,
   },
   breakpoints: {
+    768: {
+      slidesPerView: 3,
+    },
     1024: {
-      slidesPerView: 6,
+      slidesPerView: 4,
     },
   },
+});
+
+var swiper = new Swiper(".mySwiper-clientes", {
+  slidesPerView: 6,
+    spaceBetween: 97,
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+    breakpoints: {
+      0: {
+        slidesPerView: 3,
+      },
+      768: {
+        slidesPerView: 4,
+      },
+      1024: {
+        slidesPerView: 6,
+      },
+    },
 });
 
 var swiper = new Swiper(".mySwiper-projeto", {
